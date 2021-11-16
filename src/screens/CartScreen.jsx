00ce45f41ac4ce9e264350345545bfde0db63344
Phonekham <phone.madridista@gmail.com>
2021-11-16 10:37:30 +0700

    addToCart actions

--- src/screens/CartScreen.jsx
+++ src/screens/CartScreen.jsx
@@ -1,12 +1,22 @@
 import { useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { addToCart } from "../actions/cartActions";
 
 export default function CartScreen(props) {
-  console.log(props);
+  const dispatch = useDispatch();
   const params = useParams();
   const productId = params.id;
   const qty = props.location.search
     ? Number(props.location.search.split("=")[1])
     : 1;
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
+
   return (
     <div>
       <h1>Cart Screen</h1>
